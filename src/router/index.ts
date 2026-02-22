a04fc64b9e4ab7924471b55d1a9bfa6a5d270bde
Beela303 <devbeela@gmail.com>
2026-02-22 19:20:46 +0100

    changed file names and routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import TabsPage from '../views/TabsPage.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/tabs/tasks'
   },
   {
     path: '/tabs/',
@@ -13,19 +13,19 @@ const routes: Array<RouteRecordRaw> = [
     children: [
       {
         path: '',
-        redirect: '/tabs/tab1'
+        redirect: '/tasks'
       },
       {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
+        path: 'tasks',
+        component: () => import('@/views/tasks.vue')
       },
       {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
+        path: 'add',
+        component: () => import('@/views/add.vue')
       },
       {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        path: 'statistics',
+        component: () => import('@/views/statistics.vue')
       }
     ]
   }
